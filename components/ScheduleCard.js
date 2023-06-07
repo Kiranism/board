559a976f4da588881321dc260307c943a265e47f
Kiranism <iamkiran2831@gmail.com>
2023-06-08 02:59:10 +0530

    initial commit

--- components/ScheduleCard.js
+++ components/ScheduleCard.js
@@ -4,7 +4,7 @@ const ScheduleCard = () => {
   return (
     <div className="bg-white text-black rounded-2xl px-4 py-3 flex-1">
       <div className="flex justify-between items-center px-3 py-2 mb-5">
-        <h2 className="font-bold text-xl">Today's schedule</h2>
+        <h2 className="font-bold text-xl">Today&apos;s schedule</h2>
         <p className="text-sm text-gray-700">See All</p>
       </div>
 
